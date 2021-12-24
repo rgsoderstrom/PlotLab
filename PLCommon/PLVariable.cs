31cd136a3e07cc2ed20329f4930ee06bdafaeccb
Randy Soderstrom <rgsoderstrom@gmail.com>
2021-12-23 19:52:55 -0800

    added parens to string returned by ToString ()

--- PLCommon/PLVariable.cs
+++ PLCommon/PLVariable.cs
@@ -565,7 +565,7 @@ namespace PLCommon
 
             foreach (PLVariable var in Data)
             {
-                str += var.ToString () +"\n";
+                str += "(" + var.ToString () + ")" + "\n";
             }
 
             return str;
