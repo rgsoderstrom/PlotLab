b222db5df06aeb517d4488d4975015163d02f061
Randy Soderstrom <rgsoderstrom@gmail.com>
2023-02-06 07:47:03 -0800

    add directories with new test cases to startup path

--- Examples/startup.m
+++ Examples/startup.m
@@ -6,10 +6,10 @@ addpath ('C:\Users\rgsod\Documents\FreeMat\eigenchris\TFB_Covectors');
 addpath ('C:\Users\rgsod\Documents\FreeMat\eigenchris\TFB_LinearMaps');
 addpath ('C:\Users\rgsod\Documents\FreeMat\eigenchris\TFB_MetricTensors');
 
-% addpath ('C:\Users\rgsod\Documents\Visual Studio 2019\Projects\PlotLab\Examples\FuncsAndScripts1');
-% addpath ('C:\Users\rgsod\Documents\Visual Studio 2019\Projects\PlotLab\Examples\FuncsAndScripts2');
 addpath ('C:\Users\rgsod\Documents\Visual Studio 2022\Projects\PlotLab\Examples\TestCases');
 addpath ('C:\Users\rgsod\Documents\Visual Studio 2022\Projects\PlotLab\Examples\TestCases\PlotTests');
+addpath ('C:\Users\rgsod\Documents\Visual Studio 2022\Projects\PlotLab\Examples\TestCases\ContourPlots');
+addpath ('C:\Users\rgsod\Documents\Visual Studio 2022\Projects\PlotLab\Examples\TestCases\VectorPlots');
 
 addpath ('C:\Users\rgsod\Documents\Visual Studio 2022\Projects\PlotLab\Examples\Utils');
 
