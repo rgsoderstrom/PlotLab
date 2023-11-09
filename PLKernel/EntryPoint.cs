5cb458b09aead1f73bb3f3b12f6da13535e7f9e8
Randy Soderstrom <rgsoderstrom@gmail.com>
2023-11-08 18:28:06 -0800

    Restore some old debug code while making it more useable

--- PLKernel/EntryPoint.cs
+++ PLKernel/EntryPoint.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows;
+using System.Windows.Controls;
 using PLCommon;
 using PLWorkspace;
 
@@ -11,17 +12,48 @@ namespace PLKernel
 {
     public class EntryPoint
     {
+        static public bool ShowParsingTokens = false;
+        static public bool ShowExprTree      = false;
+        static int Counter = 1;
+
         public void ProcessArithmeticExpression (ref PLVariable answer, string expression, Workspace workspace)
         {
             try
             {
-                //TokenParsing parsing = new TokenParsing ();
-                //List<Token> passOneTokens = parsing.ParsingPassOne (expression);
-                //List<Token> passTwoTokens = parsing.ParsingPassTwo (passOneTokens, workspace);
+                if (ShowParsingTokens || ShowExprTree) Counter++;
+
+                if (ShowParsingTokens)
+                {
+                    TokenParsing parsing = new TokenParsing ();
+                    List<Token> passOneTokens = parsing.ParsingPassOne (expression);
+                    List<Token> passTwoTokens = parsing.ParsingPassTwo (passOneTokens, workspace);
+
+                    Window win = new Window ();
+                    TextBox tb = new TextBox ();
+                    tb.Text += "First pass:\n";
+                    foreach (Token tok in passOneTokens) tb.Text += tok.ToString () + "\n";
+                    tb.Text += "\nSecond pass:\n";
+                    foreach (Token tok in passTwoTokens) tb.Text += tok.ToString () + "\n";
+                    win.Content = tb;
+                    win.Title = "Parsing " + Counter;
+                    win.Width = 400;
+                    win.Height = 300;
+                    win.Show ();
+                }
 
                 ExpressionTree tree = new ExpressionTree (expression, workspace);
-                //window.TreeView1.Items.Clear ();
-                //window.TreeView1.Items.Add (tree.TreeView ());
+
+                if (ShowExprTree)
+                {
+                    Window win = new Window ();
+                    TreeView tv = new TreeView ();
+                    tv.Items.Add (tree.TreeView ());
+                    win.Content = tv;
+                    win.Title = "Tree " + Counter;
+                    win.Width = 400;
+                    win.Height = 300;
+                    win.Show ();
+                }
 
                 answer = tree.Evaluate (workspace);
             }
