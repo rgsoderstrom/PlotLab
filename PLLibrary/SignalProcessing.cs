4c604b8d686982df626346aab5765f2997c3f502
Randy Soderstrom <rgsoderstrom@gmail.com>
2023-12-23 16:45:10 -0800

    Begin adding interfaces to Math.Net signal processing methods

---
+++ PLLibrary/SignalProcessing.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+using PLCommon;
+
+//using PerformanceAnalysis.FFT;
+//using PerformanceAnalysis.LinearAlgebra;
+//using MathNet.Numerics.Transformations;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Filtering;
+using System.IO;
+using MathNet.Filtering.FIR;
+//using static System.Net.WebRequestMethods;
+
+namespace FunctionLibrary
+{
+    static public partial class SignalProcessing
+    {
+        //*********************************************************************************************
+        //
+        // map function name strings to executable functions
+        //
+
+        static public Dictionary<string, PLFunction> GetSignalProcessingContents ()
+        {
+            return new Dictionary<string, PLFunction> 
+            {
+                {"CreateLPF",    CreateLPF},
+                {"RunFilter",    RunFilter},
+                {"ClearFilter",  ClearFilter},
+                {"DeleteFilter", DeleteFilter},
+            };
+        }
+
+
+
+    }
+
+
+}
