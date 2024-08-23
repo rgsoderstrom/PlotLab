72027b4dc79c76f9805017a5e2a43d690d4b685e
Randy Soderstrom <rgsoderstrom@gmail.com>
2024-08-23 07:54:45 -0700

    Add raised cosine window

--- Examples/FuncsAndScripts1/SignalProc/A2D_Tests.m
+++ Examples/FuncsAndScripts1/SignalProc/A2D_Tests.m
@@ -3,13 +3,25 @@ addpath C:\Users\rgsod\Documents\Visual Studio 2022\Projects\ArduinoSupport\SONA
         
 clear
 		
-samples2 ; figure (1) ; clf ; title (''); hold off
+samples2 ; figure (2) ; clf ; title (''); hold off
 
 samples = z';
-fftResults = fft (samples, Fs);
+
+% window
+L = length (samples);
+phase = linspace (0, 2 * pi, L);
+win = 0.5 * (1 - cos (phase));
+
+dc = mean (samples);
+samples = samples - dc;
+
+
+%fftResults = fft (samples, Fs);
+fftResults = fft (win .* samples, Fs);
 
 freqScale  = fftResults (1, :);
 magSquared = fftResults (2, :);
+phase      = fftResults (3, :);
 
 peakMag2 = 0;
 
@@ -24,12 +36,16 @@ dB = 10 * log10 (magSquared);
 
 plot (freqScale, dB);
 
+%return
+
 for i=513:1023,
 	A = (dB (i) > dB (i-1));
 	B = (dB (i) > dB (i+1));
 	C = (dB (i) > -55);
 	
 	if A && B && C
-		disp (freqScale (i));
+		%dd = [(freqScale (i)) (sqrt (magSquared (i) * peakMag2)) (phase (i))];
+		%disp (dd);
+	    disp (freqScale (i));
 	end
 end
