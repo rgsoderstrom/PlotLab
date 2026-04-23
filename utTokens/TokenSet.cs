4ffa5642335f4bcdee3c3dfdef3e2c7acf86da3a
Randy Soderstrom <rgsoderstrom@gmail.com>
2026-04-23 07:45:06 -0700

    add TokenSet class

---
+++ utTokens/TokenSet.cs
@@ -0,0 +1,32 @@
+﻿
+/*
+    TokenSet - list of Tokens and any properties common to all
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    public class TokenSet
+    {
+        private List<IToken> tokens = new List<IToken> ();
+        public int Count {get {return tokens.Count;}}
+
+        private bool suppessPrinting = false;
+        public  bool SuppressPrinting {get {return suppessPrinting;} private set {suppessPrinting = value;}}
+
+        //**************************************************************************
+
+        // ctors
+
+        public TokenSet ()
+        {
+
+        }
+
+    }
+}
