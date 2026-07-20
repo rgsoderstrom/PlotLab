7b7b50177c1e5d494c7d366fb4915fee9d9a3c8a
Randy Soderstrom <rgsoderstrom@gmail.com>
2026-07-20 06:47:31 -0700

    Disable debug tree display

--- utExpressionTree/MainWindow.xaml.cs
+++ utExpressionTree/MainWindow.xaml.cs
@@ -69,7 +69,7 @@ namespace utExpressionTree
 
                             //**********************************************************************
 
-                            if (true) // show token parsing
+                            if (false) // show token parsing
                             { 
                                 // first pass
                                 TokenParsing parsing = new TokenParsing ();
@@ -99,7 +99,7 @@ namespace utExpressionTree
 
                             ExpressionTree tree = new ExpressionTree (annotated);
 
-                            if (true) // show expression tree
+                            if (false) // show expression tree
                             { 
                                 Window win2 = new Window ();
                                 TreeView tv = new TreeView ();
