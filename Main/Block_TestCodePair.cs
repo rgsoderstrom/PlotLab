5fae404646daed5c3ccbc5523b108b822d18eced
Randy Soderstrom <rgsoderstrom@gmail.com>
2026-07-27 06:21:08 -0700

    Move class TestCodePair into its own file

---
+++ Main/Block_TestCodePair.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLMain
+{
+    // TestCodePair - a component of various blocks
+    //  - "if" source code is split into a list of these
+
+    internal class TestCodePair
+    {
+        readonly internal string test;
+        readonly internal List<AnnotatedString> code;
+        
+        internal TestCodePair (string str) 
+        {
+            test = str; 
+            code = new List<AnnotatedString> ();
+        }   
+            
+        internal TestCodePair () : this ("true") 
+        {
+        } 
+
+        internal void Add (AnnotatedString astr) 
+        {
+            code.Add (astr);
+        }
+
+        public override string ToString ()
+        {
+            string str = "Test: " + test;
+
+            foreach (AnnotatedString astr in code)
+                str += "\n      " + astr.Plain;
+
+            str += "\n";
+            return str;
+        }
+    }
+}
