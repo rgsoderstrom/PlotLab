b5df5c818cb61efb3cf21c12908726db0d7dbadc
Randy Soderstrom <rgsoderstrom@gmail.com>
2026-08-17 07:34:54 -0700

    remove debug prints, fix comments

--- Main/StringClassifier.cs
+++ Main/StringClassifier.cs
@@ -3,21 +3,11 @@
         - classify a single input string
 */
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using PLCommon;
 using PLSystem;
 using PLFileSystem;
 using PLLibrary;
 using PLWorkspace;
-
-
-// all Block references commented out
-
 
 namespace PLMain
 {
@@ -34,35 +24,26 @@ namespace PLMain
         //
         public InputLineType Classify (AnnotatedString astr)
         {
-            // anything we don't know what to do with will be passed to the expression tree
-            InputLineType defaultType = InputLineType.Unknown;//ExpressionTree;
-
-            //Console.WriteLine ("A");
+            // anything remaining unknown will be passed to the expression tree
+            InputLineType defaultType = InputLineType.Unknown;
 
             // for lines with alphanumeric only
             if (astr.AlphanumericOnly)
             {
-                //Console.WriteLine ("B");
-
                 // if it's a single word, check variables and scripts
                 if (astr.SingleWord)
                 {
-                    //Console.WriteLine ("C");
-
                     if (Workspace.WhatIs (astr.Plain) == SymbolicNameTypes.Variable)
                         return InputLineType.VariableName;
 
                     if (FileSystem.IsScriptFile (astr.Plain))
                         return InputLineType.ScriptFile;
 
-                    //Console.WriteLine ("ask BM");
-
                     if (BlockManager.IsBlockName (astr.Plain))
                         return InputLineType.BlockName;
                 }
             }
 
-            // alphanumeric only but more than one word
             string FirstWord = astr.FirstWord;
 
             // see if the first word is a system command
